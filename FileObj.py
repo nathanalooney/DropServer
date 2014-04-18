65de8822deb03985bd6f1fcbe8138c2d917f8b8e
kbroderi <kbroder109@gmail.com>
2014-04-18 13:57:24 -0400

    Create FileObj.py
    added the file object class

---
+++ FileObj.py
@@ -0,0 +1,14 @@
+__author__ = 'Kevin'
+class fileObj:
+
+    def __init__(self, path, time):
+        self.path = path
+        self.time = time
+        self.temp_time = 0
+
+    def setID(self, ID):
+        self.ID = ID
+
+    def setTempID(self, tempID):
+        self.tempID = tempID
+
