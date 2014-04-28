8bff27fc061ea78f85738daa5d1d2c4bbcae1aa2
nal3vm <nal3vm@virginia.edu>
2014-04-27 23:19:15 -0400

    Update urls.py

--- syncfolder/urls.py
+++ syncfolder/urls.py
@@ -5,7 +5,12 @@ from syncfolder import views
 urlpatterns = patterns('',
 
 	url(r'^$', views.index, name='index'),
-	url(r'^send$', views.send, name='send'),
-	url(r'^remove$', views.remove, name='remove'),
+	url(r'^fileCreate$', views.fileCreate, name='fileCreate'),
+	url(r'^dirCreate$', views.dirCreate, name='dirCreate'),
+	url(r'^fileDelete$', views.fileDelete, name = 'fileDelete'),
+	url(r'^dirDelete$', views.dirDelete, name='dirDelete'),
 	url(r'^update$', views.update, name='update'),
+	url(r'^move$', views.move, name='move'),
+	url(r'^pull$', views.pull, name='pull'),
+	url(r'^getServerIndex$', views.getServerIndex, name='getServerIndex'),
 )
